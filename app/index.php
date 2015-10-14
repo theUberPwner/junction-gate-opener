4fe96b9a3898ba03de5fbfdd43d3f0334ef19a8e
Steven Shelby <stevenshelby08@gmail.com>
2015-10-13 21:39:30 -0400

    config path

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/config.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config.php';
 
 $app = new Silex\Application();
 
